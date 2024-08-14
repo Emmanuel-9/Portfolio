005904cb8ddd1aedee6a6061bd17ca0eebeab2ee
Emmanuel-9 <oregadaniel181@gmail.com>
2024-08-14 09:48:11 +0300

    Add the respective CSS and JS files.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/* ==  Typing Animation  == */
+var typed = new Typed(".typing", {
+    strings:["Full-Stack Developer","Odoo Functional Consultant", "Junior DevOps"],
+    typeSpeed: 100,
+    BackSpeed: 8,
+    loop:true
+})
